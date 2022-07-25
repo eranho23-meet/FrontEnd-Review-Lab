8a4978c5ffb17ded423c63b82915a51b93f98ee8
eranho23-meet <109900208+eranho23-meet@users.noreply.github.com>
2022-07-25 16:14:55 +0300

    made color switching function
    made a cool and eppik color switching function which changes the background a bit

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function changeBackgroundColor(color){
-	body = document.getElementsByTagName(body)
-	body.style.backgroundColor = color
+var count = 0;
+
+function changeBackgroundColor(){
+	count++;
+	if (count % 4 === 1) {
+		document.body.style.backgroundColor = "#ad82cf";
+	}
+	else if (count % 4 === 2) {
+		document.body.style.backgroundColor = "#663399";
+	}
+	else if (count % 4 === 3) {
+		document.body.style.backgroundColor = "#9259a8";
+	}
+	else {
+		document.body.style.backgroundColor = "#debeef";
+	
+	}
 }
